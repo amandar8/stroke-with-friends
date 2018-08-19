a775ad2dbb89079aff210033c499c6f43b9dbcb0
Amandar8 <amandaruth08@gmail.com>
2018-08-19 12:18:17 -0500

    working on user route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const knex = require('knex')(config);
+const config = require('../knexfile')[env];
+const env = process.env.NODE_ENV || 'development';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/login', function(req, res, next) {
+
 });
 
 module.exports = router;
