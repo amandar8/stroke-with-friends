5573ac0d38715ab1eb18d0296eea7813433e7153
edpantzar@gmail.com <edpantzar@gmail.com>
2018-08-20 10:49:00 -0500

    Removed index routing

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,26 @@
 var express = require('express');
 var router = express.Router();
-const knex = require('knex')(config); // define database based on above
-const config = require('../knexfile')[env]; // pull in correct db with env configs
 const env = process.env.NODE_ENV || 'development'; // set environment
+const config = require('../knexfile')[env]; // pull in correct db with env configs
+const knex = require('knex')(config); // define database based on above
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
-router.post('/', function(req, res, next) {
-  if (req.body.username && req.body.password && req.body.alias) {
-    knex('users')
-    .where('username', req.body.username)
-    .then((user) => {
-        res.render('profile', {user: user});
-    })
-  }
-});
+// router.post('/', function(req, res, next) {
+//   if (req.body.username && req.body.password && req.body.alias) {
+//     knex('users')
+//     .where('username', req.body.username)
+//     .then((user) => {
+//         res.render('profile', {user: user});
+//     })
+//   }
+// });
 
-router.post('/', function(req, res, next) {
-  const user = {}
-});
+// router.post('/', function(req, res, next) {
+//   const user = {}
+// });
 
 module.exports = router;
