8e8cb0efad6022b1dbd2ffad542c4383290638e7
edpantzar@gmail.com <edpantzar@gmail.com>
2018-08-21 13:11:39 -0500

    Added admin component framework

---
+++ client/src/Admin.js
@@ -0,0 +1,128 @@
+import React, {Component} from 'react';
+
+function UserEdit() {
+  return (
+    <form className="mb-4">
+      <div className="form-group">
+        <label htmlFor="exampleInputEmail1">Username</label>
+        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="exampleInputPassword1">Password</label>
+        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+      </div>
+      <button type="submit" className="btn btn-secondary btn-sm">Submit</button>
+    </form>
+  );
+}
+
+function GroupEdit() {
+  return (
+    <form className="mb-4">
+      <div className="form-group">
+        <label htmlFor="exampleInputEmail1">Group Name</label>
+        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="exampleInputPassword1">Password</label>
+        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+      </div>
+      <button type="submit" className="btn btn-secondary btn-sm">Submit</button>
+    </form>
+  );
+}
+
+class Admin extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      adminRights: props.isAdmin,
+      editUserHidden: true,
+      editGroupHidden: true,
+    }
+    this.userEditHidden = this.userEditHidden.bind(this);
+    this.groupEditHidden = this.groupEditHidden.bind(this);
+  }
+
+  userEditHidden() {
+    this.setState({
+      editUserHidden: !this.state.editUserHidden,
+      editGroupHidden: true
+    })
+  }
+
+  groupEditHidden() {
+    this.setState({
+      editGroupHidden: !this.state.editGroupHidden,
+      editUserHidden: true
+    })
+  }
+
+  render(){
+    let userList = [];
+    for(let i=0; i<10; i++){
+      userList.push('username');
+    }
+    let groupList = [];
+    for(let i=0; i<5; i++){
+      groupList.push('GroupName');
+    }
+    return(
+      <div className="row d-flex justify-content-center">
+        <div className="col-11">
+          <h3>Admin Panel</h3>
+          <div className='row'>
+            <div className="col-12 mb-4">
+              <button className="btn btn-default btn-sm" onClick={this.userEditHidden}>Edit User</button>
+              <button className="btn btn-default btn-sm" onClick={this.groupEditHidden}>Edit Group</button>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col-6'>
+              {this.state.editUserHidden ? null : <UserEdit />}
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col-6'>
+              {this.state.editGroupHidden ? null : <GroupEdit />}
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col-4'>
+              <h6>Registered users</h6>
+              <ul>
+                {
+                  userList.map((user, index) =>{
+                    return <li key={index}>{user}</li>;
+                  })
+                }
+              </ul>
+            </div>
+            <div className='col-4'>
+              <h6>Active Users</h6>
+              <ul>
+                {
+                  userList.map((user, index) =>{
+                    return <li key={index}>{user}</li>;
+                  })
+                }
+              </ul>
+            </div>
+            <div className='col-4'>
+              <h6>Groups</h6>
+              <ul>
+                {
+                  groupList.map((group, index) =>{
+                    return <li key={index}>{group}</li>;
+                  })
+                }
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Admin;
