2ba6bfa85c25df902a987d426da133eee6b35dee
edpantzar@gmail.com <edpantzar@gmail.com>
2018-08-23 15:17:52 -0500

    Moved sockets to express

--- server.js
+++ server.js
@@ -14,7 +14,34 @@ const methodOverride = require('method-override');
 const port = process.env.PORT || 5000;
 
 const app = express();
-const server = require('http').createServer(app);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+// event handler for incoming connections
+io.on('connection', (socket) => {
+
+    for (let i in line_history) {
+        socket.emit('draw_line', {
+            line: line_history[i]
+            
+        });
+        console.log(line_history[i]);
+    }
+
+    // add handler "draw_line" event.
+    socket.on('draw_line', (data) => {
+
+        //add recieved line to history
+        line_history.push(data.line);
+
+        // send line to all clients
+        io.emit('draw_line', {
+            line: data.line
+        });
+        console.log(data.line);
+
+    });
+});
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
