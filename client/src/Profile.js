0a5cba7665ca230770ed2dc314c4b524c415c15c
Jake Lewis <jakeaustinlewis@gmail.com>
2018-08-26 16:39:41 -0500

    updated profile page

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import './Profile.css'
 import Edit from './Edit.js';
-
-
-
 class Profile extends Component {
     constructor(props){
         super(props);
@@ -17,20 +14,64 @@ class Profile extends Component {
     logout() {
         this.props.auth.logout();
     }
-
+    // componentDidMount() {
+    //     if (this.state.id !== undefined && this.state.id !== null){
+    //         fetch(`/users/id/${this.state.id}`, { 
+    //             method: 'GET',
+    //             mode: 'cors',
+    //             headers: {
+    //                 "Content-Type": "application/json; charset=utf-8"
+    //             },
+    //             redirect: "follow",
+    //             referrer: "no-referrer"
+    //         })
+    //         .then(responce => responce.json())
+    //         .then(data => this.setState({data: data}))
+    //         .catch(error => console.error('Error:', error));
+    //     }
+    // }
     render() {
         return (
             <div>
            <header className="App-header text-dark">
              <h3>User Profile</h3>
-             <a href="/" onClick={(event) => this.props.showWelcome(event)}>Return Home</a><br />
-             <a href="/" onClick={(event) => (this.props.showCanvas(event))}>Go to Canvas</a>
+             <a href="/" onClick={(event) => this.props.showWelcome(event)}>Return Home</a>
            </header>
            <div className="container-fluid p-0 ">
              <div className="row bodySize m-0">
-               <div className="col-3 blue p-0 "> 
-                 <img src="https://picsum.photos/200/300?man" className="fullWidth profilePictureSize"></img>
-               </div>
+               <nav className="col-3 p-0 border-right sidebar"> 
+                    <div class="sidebar-header d-flex align-items-center justify-content-center p-3">
+                      <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/25613_425815345448_7787727_n.jpg?_nc_cat=0&oh=8a84ca4ef69335e4f208c12d77db47ab&oe=5C06665E" className="fullWidth profilePictureSize"></img> 
+                    </div>
+                    <ul class="list-unstyled components">
+                        <li>
+                            <a href="/clientHome" class="d-flex align-items-center sidebarFontWeight">
+                                <i class="fas fa-user mr-2"></i>
+                                Profile</a>
+                        </li>
+                        <li>
+                            <a href="#" class="d-flex align-items-center">
+                                <i class="fas fa-user mr-2"></i>
+                                Past Canvases</a>
+                        </li>
+                        <li>
+                            <a href="#" class="d-flex align-items-center">
+                                <i class="fas fa-paint-brush mr-2"></i>
+                                New Strokes</a>
+                        </li>
+                        <li>
+                            <a href="#" class="d-flex align-items-center">
+                                <i class="fas fa-users mr-2"></i>
+                                Join A Group</a>
+                        </li>
+                        <li>
+                            <a href="" class="d-flex align-items-center">
+                                <i class="fas fa-sign-out-alt mr-2"></i>
+                                Log Out</a>
+                        </li>
+                    </ul>
+                </nav>
+         
                <div className="col-9 p-0">
                  <div className="container-fluid fullHeight p-0">
                  <div className="row fullHeight d-flex justify-content-center">
@@ -40,30 +81,26 @@ class Profile extends Component {
                     <div>
                        <div>Username
                        </div>
-                        <div className="profileInfoColor d-flex"><div className="icon-backgroundColor"><i className="fas fa-user-alt mx-2"></i></div>{this.props.activeUser.username}
+                        <div className="profileInfoColor d-flex"><div className="icon-backgroundColor mr-3"><i className="fas fa-user-alt mx-2"></i></div>this.props.activeUser.username
                         </div>
                     </div>
                     
                     <div>
                        <div>Name
                        </div>
-                       <div className="profileInfoColor d-flex"><div className="icon-backgroundColor"><i className="fas fa-envelope mx-2"></i></div>{this.props.activeUser.name}
+                       <div className="profileInfoColor d-flex"><div className="icon-backgroundColor mr-3"><i className="fas fa-envelope mx-2"></i></div>this.props.activeUser.email
                       </div>
                     </div>
-
                      <div>
                        <div>Alias
                        </div>
-                       <div className="profileInfoColor d-flex"><div className="icon-backgroundColor"><i className="fas fa-shield-alt mx-2"></i></div>{this.props.activeUser.alias}
+                       <div className="profileInfoColor d-flex"><div className="icon-backgroundColor mr-3"><i className="fas fa-shield-alt mx-2"></i></div>this.props.activeUser.alias
                        </div>
                     </div>
-
-
                    {/* </section> */}
                    </div>
                    </div>
                  </div>
-
                </div>
              </div>
            </div>
@@ -80,5 +117,4 @@ class Profile extends Component {
         );
     }
 }
-
 export default Profile;
